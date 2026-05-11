237498c8c31e15a442afb66631e13bd26e5b68a0
DymShanks <00dcs00@gmail.com>
2026-05-11 18:04:34 +0530

    Add RISC-V GPR hazard coverage tests

---
+++ generators/coverage/src/covergroupgen/templates/cp_gpr_hazard_w.sv
@@ -0,0 +1,5 @@
+    cp_gpr_hazard_w : coverpoint check_gpr_hazards(ins.hart, ins.issue)  iff (ins.trap == 0 )  {
+        //GPR write hazard
+        bins hazards[]  = {NO_HAZARD, WAW_HAZARD, WAR_HAZARD};
+    }
+
