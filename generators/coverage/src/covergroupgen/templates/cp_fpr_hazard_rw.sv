86de6521f4e64eb3f06268db436a3a15cd1ff264
Divyam Shankhdhar <00dcs00@gmail.com>
2026-05-21 15:34:09 +0530

    fix(hazard): improve RAW/WAW test quality per David's review

--- generators/coverage/src/covergroupgen/templates/cp_fpr_hazard_rw.sv
+++ generators/coverage/src/covergroupgen/templates/cp_fpr_hazard_rw.sv
@@ -1,4 +1,4 @@
-    cp_fpr_hazard_rw : coverpoint check_fpr_hazards(ins.hart, ins.issue)  iff (ins.trap == 0 )  {
+    cp_fpr_hazard_rw : coverpoint check_fpr_hazards(ins.hart, ins.issue, 1)  iff (ins.trap == 0 )  {
         //FPR hazard
         bins hazards[]  = {NO_HAZARD, RAW_HAZARD, WAW_HAZARD, WAR_HAZARD};
     }
